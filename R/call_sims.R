84d108cfcbabdc14d8e158aff2ae76a10a45b4d9
Jeremiah Jones <j.miahjones@gmail.com>
2021-12-14 09:32:42 -0500

    More extensive results for the positivity violation setting.

--- R/call_sims.R
+++ R/call_sims.R
@@ -5,40 +5,27 @@ library(dplyr)
 library(purrr)
 library(qs)
 
-rho <- c(0, 0.25, 0.5, 0.75)
-
 base_params <- tribble(
   ~n,   ~p,   ~num_simulations,
   200,  100,  100,
   500,  200,  100
 )
 
-vary_params <- tidyr::expand_grid(rho=c(0, 0.25, 0.5, 0.75), 
-  sig_x=seq(.2, 1, by=.2), 
-  scenario=1, 
-  # method=factor(1:4, levels=1:4, 
-  #   labels=c(
-  #     "OAL", "OAL+overlap", "GOALn", "GLM"
-  #   )
-  # ),
-  sel_method=factor(1:2, levels=1:2, labels=c("OAL", "GOALn")),
-  est_method=factor(1:5, levels=1:5,
-    labels=c("IPW", "AIPW", "Overlap", "Trunc", "TMLE")
-  )
+vary_params <- tidyr::expand_grid(
+  rho = 0.75,
+  sig_x = 1,
+  scenario = 1, 
+  sel_method = factor(1:2, levels = 1:2, 
+    labels = c("OAL", "GOALn"))
 )
 
 params <- tidyr::expand_grid(base_params, vary_params)
-# params <- params %>%
-#   mutate(use_ridge = (method == "GOALn"),
-#          use_overlap = (method == "OAL+overlap"),
-#          use_glm = (method == "GLM"))
-
 
 source("./R/simulate_oal.R")
 
 results <- params %>% 
   mutate(
-    data=pmap(., simulate_oal)
+    data = pmap(., simulate_oal)
   )
 
-qsave(results, file="./results/full_results.qs")
+qsave(results, file="./results/extensive_results.qs")
